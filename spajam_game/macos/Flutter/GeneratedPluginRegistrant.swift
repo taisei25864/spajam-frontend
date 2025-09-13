74e138450555bf764852942848b072828ac9c8d5
yuto <yuto@yuto.yuto>
2025-09-14 04:06:51 +0900

    update pubspec.yaml

--- spajam_game/macos/Flutter/GeneratedPluginRegistrant.swift
+++ spajam_game/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import flutter_webrtc
 import path_provider_foundation
+import record_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
 }
