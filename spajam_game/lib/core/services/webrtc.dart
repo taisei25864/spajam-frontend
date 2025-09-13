9ef456499ed702134d76f6bf38b187de70a8b43a
Tetsu-is <hap30305@kwansei.ac.jp>
2025-09-13 21:25:39 +0900

    temp

--- spajam_game/lib/core/services/webrtc.dart
+++ spajam_game/lib/core/services/webrtc.dart
@@ -1,1 +1,49 @@
-import 
+import 'websocket.dart';
+
+final String _host = 'localhost';
+final int _port = 8000;
+
+// connecting signaling server
+Future<void> connect() async {
+    var url = 'https://$_host:$_port/ws';
+    _socket = SimpleWebSocket(url);
+
+    print('connect to $url');
+
+    if (_turnCredential == null) {
+      try {
+        _turnCredential = await getTurnCredential(_host, _port);
+        _iceServers = {
+          'iceServers': [
+            {
+              'urls': _turnCredential['uris'][0],
+              'username': _turnCredential['username'],
+              'credential': _turnCredential['password']
+            },
+          ]
+        };
+      } catch (e) {}
+    }
+
+    _socket?.onOpen = () {
+      print('onOpen');
+      onSignalingStateChange?.call(SignalingState.connectionOpen);
+      _send('new', {
+        'name': DeviceInfo.label,
+        'id': _selfId,
+        'user_agent': DeviceInfo.userAgent
+      });
+    };
+
+    _socket?.onMessage = (message) {
+      print('Received data: ' + message);
+      onMessage(_decoder.convert(message));
+    };
+
+    _socket?.onClose = (int? code, String? reason) {
+      print('Closed by server [$code => $reason]!');
+      onSignalingStateChange?.call(SignalingState.connectionClosed);
+    };
+
+    await _socket?.connect();
+  }
